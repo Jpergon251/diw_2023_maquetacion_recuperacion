a73eb65a29171194034e5af47e9f03b0e086f22c
Jpergon251 <Jpergon251@g.educaand.es>
2023-05-21 19:47:34 +0200

    paginas hechas

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-    presets: ["@vue/cli-plugin-babel/preset"],
-    env: {
-      test: {
-        plugins: ["transform-require-context"],
-      },
-    },
     pluginOptions: {
       parcel: {
-        // Agrega los estilos globales en main.sass
-        // para que sean aplicados en todos los componentes
         css: {
           files: [
             {
